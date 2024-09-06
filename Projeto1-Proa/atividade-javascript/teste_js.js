6861530989233534f2316e17fa893c45a0b21c2b
yanrafael <89853027+yanrafael@users.noreply.github.com>
2024-09-06 09:21:08 -0300

    adicionei novas atividades

---
+++ Projeto1-Proa/atividade-javascript/teste_js.js
@@ -0,0 +1,23 @@
+function show_camada1(){
+    document.getElementById("camada1").style.visibility="visible";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="hidden";
+}
+
+function show_camada2(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="visible";
+    document.getElementById("camada3").style.visibility="hidden";
+}
+
+function show_camada3(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="visible";
+}
+
+function some_tudo(){
+    document.getElementById("camada1").style.visibility="hidden";
+    document.getElementById("camada2").style.visibility="hidden";
+    document.getElementById("camada3").style.visibility="hidden";
+}
